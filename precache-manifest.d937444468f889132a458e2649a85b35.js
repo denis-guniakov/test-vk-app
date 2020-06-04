6a0ad9982ed2fd5e2a0ee0e6ec72fd54bf611f55
Denis Gunyakov <emploise@mail.ru>
2020-06-04 08:57:13 +0700

    Updates

---
+++ precache-manifest.d937444468f889132a458e2649a85b35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b65c86ab097f36c8555592aa9093e15",
+    "url": "/test-vk-app/index.html"
+  },
+  {
+    "revision": "2ad5d3b53793ad9248eb",
+    "url": "/test-vk-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "59292abcdc25e8bc4bc3",
+    "url": "/test-vk-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2ad5d3b53793ad9248eb",
+    "url": "/test-vk-app/static/js/2.44e1af72.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-vk-app/static/js/2.44e1af72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59292abcdc25e8bc4bc3",
+    "url": "/test-vk-app/static/js/main.754d0f2c.chunk.js"
+  },
+  {
+    "revision": "015ed8784cd0c81295ab",
+    "url": "/test-vk-app/static/js/runtime-main.43d04716.js"
+  }
+]);
